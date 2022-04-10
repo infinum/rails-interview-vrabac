149e5186c39b595bd0f568fe59f5127c82de25c5
Vedran Hrncic <vedran.hrncic@infinum.hr>
2022-04-10 21:52:58 +0200

    add line item model

---
+++ app/models/line_item.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :integer          not null, primary key
+#  quantity   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  order_id   :integer          not null
+#  product_id :integer          not null
+#
+# Indexes
+#
+#  index_line_items_on_order_id    (order_id)
+#  index_line_items_on_product_id  (product_id)
+#
+# Foreign Keys
+#
+#  order_id    (order_id => orders.id)
+#  product_id  (product_id => products.id)
+#
+class LineItem < ApplicationRecord
+  belongs_to :order
+  belongs_to :product
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_10_195004) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_10_195120) do
+  create_table "line_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer "total_price_cents", null: false
     t.datetime "created_at", null: false
@@ -26,4 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_10_195004) do
     t.index ["code"], name: "index_products_on_code", unique: true
   end
 
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
 end
